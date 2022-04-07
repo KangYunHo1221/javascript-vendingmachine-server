03d55d3ddbeb0cc9f95aa3ec15ef72cc3852a98b
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-04-07 12:24:17 +0900

    cors 세팅

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
+const cors = require('cors');
 
 const app = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -16,6 +17,7 @@ const rules = auth.rewriter({
 });
 
 // You must apply the auth middleware before the router
+app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
